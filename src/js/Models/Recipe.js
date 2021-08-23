3cad42868a1b006c9934fad64d9dd5e7e929144c
Adelino ARAUJO <araujo_adelino@orange.fr>
2021-08-23 09:14:12 +0100

    add Recipe class

---
+++ src/js/Models/Recipe.js
@@ -0,0 +1,81 @@
+"use strict"
+export class Recipe{
+    constructor(id, name, servings, ingredients, time,
+                description, appliance, ustensils) {
+
+        this._id = id ;
+        this._name = name ;
+        this._servings = servings ;
+        this._ingredients = ingredients ;
+        this._time = time ;
+        this._description = description ;
+        this._appliance = appliance ;
+        this._ustensils = ustensils ;
+    }
+
+    get id() {
+        return this._id;
+    }
+
+    set id(value) {
+        this._id = value;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    set name(value) {
+        this._name = value;
+    }
+
+    get servings() {
+        return this._servings;
+    }
+
+    set servings(value) {
+        this._servings = value;
+    }
+
+    get ingredients() {
+        return this._ingredients;
+    }
+
+    set ingredients(value) {
+        this._ingredients = value;
+    }
+
+    get time() {
+        return this._time;
+    }
+
+    set time(value) {
+        this._time = value;
+    }
+
+    get description() {
+        return this._description;
+    }
+
+    set description(value) {
+        this._description = value;
+    }
+
+    get appliance() {
+        return this._appliance;
+    }
+
+    set appliance(value) {
+        this._appliance = value;
+    }
+
+    get ustensils() {
+        return this._ustensils;
+    }
+
+    set ustensils(value) {
+        this._ustensils = value;
+    }
+}
+
+
