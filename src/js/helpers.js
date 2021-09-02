a77b2c69984cc94ba8656bae261e368c6a9ffb41
Adelino ARAUJO <araujo_adelino@orange.fr>
2021-09-02 11:34:49 +0100

    refactor helpers function names

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -2,24 +2,63 @@
 
 //DOM ELEMENTS
 export let gallery = document.getElementById("gallery") ;
-export let search = document.getElementById("search") ;
-export let submit = document.getElementById("submit") ;
+export let searchBar = document.getElementById("search") ;
+export let submitSearchButton = document.getElementById("submit") ;
 export let ingredientsList = document.getElementById("ingredients-list") ;
 export let devicesList = document.getElementById("devices-list") ;
 export let utensilsList = document.getElementById("utensils-list") ;
-export let ingredientsInput = document.getElementById("ingredients-input") ;
-export let devicesInput = document.getElementById("devices-input") ;
-export let utensilsInput = document.getElementById("utensils-input") ;
 export let allTags = document.getElementsByClassName("filter__tag") ;
 export let allTagsIcon = document.getElementsByClassName("filter__icon--tag") ;
 export let userSelectedTags = document.getElementById("user-selected-tags") ;
 export let displayedRecipes = document.getElementsByClassName("card") ;
+
+//TO SEARCH
+export function searchTextPatternAlgorithm(text, word) {
+    let wordLength = word.length;
+    let textLength = text.length;
+
+    for (let i = 0; i <= textLength - wordLength; i++) {
+        let j;
+
+        for (j = 0; j < wordLength; j++) {
+            if (text[i + j] !== word[j]) {
+                break;
+            }
+        }
+
+        if (j == wordLength) {
+            return true
+        }
+    }
+}
+
+export function compareUserSearchWithData(array1, array2){
+    let matchNumber = 0 ;
+
+    for (let i = 0; i < array1.length; i++) {
+        for (let j = 0; j < array2.length; j++) {
+            if (searchTextPatternAlgorithm(array1[i], array2[j])){
+                matchNumber += 1 ;
+            }
+        }
+    }
+    return matchNumber ;
+}
 
 //TO CLEAN TEXT
 let stopWords = [" a ", " au ", " aux ", " le ", " la ", " les ", " un ", " une ", " des ", " du ", " dans ", //TODO handle empty space more efficiently
     " l'", " d'", " en ", " de ", " et ", " pour ", " que ", " avec ", " plus ", " sans ", " ce ",
     " ceci ", " ou ", " mais ", " donc ", " ni ", " etc ", " recette "] ;
 
+
+export function cleanText(text){
+    text = toLowerCaseText(text) ;
+    text = removeAccents(text) ;
+    text = removePunctuation(text) ;
+    text = removeMultipleSpaces(text) ;
+    text = removeStopWords(text) ;
+    return text ;
+}
 
 function toLowerCaseText(text){
     return text.toLowerCase() ;
@@ -45,49 +84,8 @@ function removeStopWords(text){
     return text ;
 }
 
-export function cleanText(text){
-    text = toLowerCaseText(text) ;
-    text = removeAccents(text) ;
-    text = removePunctuation(text) ;
-    text = removeMultipleSpaces(text) ;
-    text = removeStopWords(text) ;
-    return text ;
-}
-
 export function splitText(text){
     return text.split(" ") ;
-}
-
-export function searchTextPattern(text, word) {
-    let wordLength = word.length;
-    let textLength = text.length;
-
-    for (let i = 0; i <= textLength - wordLength; i++) {
-        let j;
-
-        for (j = 0; j < wordLength; j++) {
-            if (text[i + j] !== word[j]) {
-                break;
-            }
-        }
-
-        if (j == wordLength) {
-            return true
-        }
-    }
-}
-
-export function arrayMatch(array1, array2){
-    let matchNumber = 0 ;
-
-    for (let i = 0; i < array1.length; i++) {
-        for (let j = 0; j < array2.length; j++) {
-            if (searchTextPattern(array1[i], array2[j])){
-                matchNumber += 1 ;
-            }
-        }
-    }
-    return matchNumber ;
 }
 
 export function removeDuplicatesFromArray(array){
