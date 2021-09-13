727ab5eb2ffd10d587c8c25d96bb5432b9451cd7
Adelino ARAUJO <araujo_adelino@orange.fr>
2021-09-13 08:02:10 +0100

    fix enableNoSearchResults message => clear previous results before

--- src/js/View.js
+++ src/js/View.js
@@ -112,6 +112,9 @@ export class View{
     }
 
     enableNoSearchResultsMessage(){
+
+        gallery.innerHTML = "" ;
+
         document
             .getElementById("no-search-results").dataset.visible = "true" ;
     }
